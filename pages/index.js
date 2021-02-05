48f93dce79ca1fae1ca994fd2786c20243b0e9dc
TarunAlgoscale <42960499+TarunAlgoscale@users.noreply.github.com>
2021-02-05 12:55:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
        <Head>
         <title>Create Next App for algoscale</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       </Head>
