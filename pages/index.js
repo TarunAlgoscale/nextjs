53a90f3aa2c13d65a08c62f7198ab280f44a31e2
TarunAlgoscale <42960499+TarunAlgoscale@users.noreply.github.com>
2021-02-05 13:21:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,52 +10,6 @@ export default function Home() {
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script>
-                    $(document).ready(function () {
-
-                var navListItems = $('div.setup-panel div a'),
-                    allWells = $('.setup-content'),
-                    allNextBtn = $('.nextBtn');
-
-                allWells.hide();
-
-                navListItems.click(function (e) {
-                    e.preventDefault();
-                    var $target = $($(this).attr('href')),
-                        $item = $(this);
-
-                    if (!$item.hasClass('disabled')) {
-                        navListItems.removeClass('btn-success').addClass('btn-default');
-                        $item.addClass('btn-success');
-                        allWells.hide();
-                        $target.show();
-                        $target.find('input:eq(0)').focus();
-                    }
-                });
-
-                allNextBtn.click(function () {
-                    var curStep = $(this).closest(".setup-content"),
-                        curStepBtn = curStep.attr("id"),
-                        nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-                        curInputs = curStep.find("input[type='text'],input[type='url']"),
-                        isValid = true;
-
-                    $(".form-group").removeClass("has-error");
-                    for (var i = 0; i < curInputs.length; i++) {
-                        if (!curInputs[i].validity.valid) {
-                            isValid = false;
-                            $(curInputs[i]).closest(".form-group").addClass("has-error");
-                        }
-                    }
-
-                    if (isValid) nextStepWizard.removeAttr('disabled').trigger('click');
-                });
-
-                $('div.setup-panel div a.btn-success').trigger('click');
-            });
-        </script>
-    
-    
       </Head>
 
         <div classname="stepwizard">
